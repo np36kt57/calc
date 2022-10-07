648ddb0d96bb950a703f31b990fcbc2bc8284f1f
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2022-10-07 17:35:00 +0200

    updated with issue-wise randomization

--- utils.js
+++ utils.js
@@ -33,16 +33,34 @@ function calc() {
     var sum = nums.reduce(function(a, b) {
         return a + b;
     }, 0);
-    var checks = nums.map(function(num) {
+    var begin_at;
+    var results = { 'issue': [], 'check': [], 'begin': [], 'direction': [] }
+    nums.forEach(function(num, i) {
         console.log("uncorrected: ", num / sum * 10);
-        return Math.round(num / sum * 10);
+        // 1: The first issue starts at the middle, moving forward.
+        // 2: The second issue starts at the middle, moving backward.
+        // 3: The third issue starts at the beginning, moving forward.
+        // 4: The fourth issue starts at the end, moving backwards.
+        if (i % 4 === 0) {
+            begin_at = 'middle: #' + Math.ceil(num / 2);
+        } else if (i % 4 === 1) {
+            begin_at = 'middle: #' + Math.floor(num / 2);
+        } else if (i % 4 === 2) {
+            begin_at = 'beginning (#1)';
+        } else {
+            begin_at = 'very end (#' + num + ')';
+        }
+        results.issue.push("no. " + (i + 1));
+        results.check.push(Math.round(num / sum * 10));
+        results.direction.push(i % 2 === 0 ? 'forward' : 'backward');
+        results.begin.push(begin_at);
     });
-    var diff = 10 - checks.reduce(function(a, b) {
+    var diff = 10 - results.check.reduce(function(a, b) {
         return a + b;
     }, 0);
     if (diff !== 0) {
         console.log("Correction: ", diff);
-        checks = checks.map(function(num) {
+        results.check = results.check.map(function(num) {
             if (diff < 0) {
                 num = num - 1;
                 diff++;
@@ -53,21 +71,50 @@ function calc() {
             return num;
         });
     }
-    diff = 10 - checks.reduce(function(a, b) {
+    diff = 10 - results.check.reduce(function(a, b) {
         return a + b;
     }, 0);
     var text = ("(Numbers given: <i>" + nums.join(', ') +
-        "</i>)<br/> <span style='color:lightgreen;font-size:120%;line-height:200%'>The distribution of the ten checks: <b>" + checks.join(', ') + "</span>");
+        "</i>)<br/> <span id='table' style='color:lightgreen;font-size:120%;line-height:200%'>The distribution of the ten results: <b></span>");
+    console.log(results);
     if (diff !== 0) {
-        console.log(text);
         feed.innerHTML = "Something went wrong in the script... Contact me please.";
     } else {
         feed.innerHTML = text;
+        results.issue.unshift('Issue number');
+        results.check.unshift('Amount to check');
+        results.begin.unshift('Begin at');
+        results.direction.unshift('Direction');
+        createTable(results);
     }
 }
 
 function remov() {
     input.value = '';
+}
+
+function createTable(data) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
+
+    Object.keys(data).forEach(function(d_key, i) {
+        var rowData = data[d_key];
+        var row = document.createElement('tr');
+        if (i == 0) {
+            row.classList.add("t_head");
+        }
+
+        rowData.forEach(function(cellData) {
+            var cell = document.createElement('td');
+            cell.appendChild(document.createTextNode(cellData));
+            row.appendChild(cell);
+        });
+
+        tableBody.appendChild(row);
+    });
+
+    table.appendChild(tableBody);
+    document.getElementById("table").appendChild(table);
 }
 
 // logo
